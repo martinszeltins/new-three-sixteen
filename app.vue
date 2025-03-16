eb47202e7b3f912d669ecb10bf26ab0c7c1cf2a2
Martins Zeltins <queeeeenz@gmail.com>
2025-03-16 10:06:59 +0200

    Initial commit

--- app.vue
+++ app.vue
@@ -4,3 +4,8 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+    // https://devtools.nuxt.com/guide/composables
+    const devtoolsClient = useNuxtDevTools() // Cannot find name 'useNuxtDevTools'.ts-plugin(2304)
+</script>
